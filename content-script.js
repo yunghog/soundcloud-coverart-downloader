6097d22572b3a8c2e5bea55c335e8fbb92d56066
Samartha <samarthahm@gmail.com>
2021-11-17 00:24:33 +0530

    basic skeleton

--- content-script.js
+++ content-script.js
@@ -1,4 +1,17 @@
-function getImage(){
-    var res = document.querySelector("#content > div > div > div.l-hero-banner > div > div.fullHero__foreground.systemPlaylistHero__foreground > div.fullHero__artwork.systemPlaylistHero__artwork > div > div > span");
-    return res
-}
+// function getImage(){
+//     var res = document.querySelector("#content > div > div > div.l-hero-banner > div > div.fullHero__foreground.systemPlaylistHero__foreground > div.fullHero__artwork.systemPlaylistHero__artwork > div > div > span").style.backgroundImage.split('"')[1];
+//     return res
+// }
+chrome.runtime.sendMessage({
+    from: 'content',
+    subject: 'showPageAction',
+  });
+  
+  chrome.runtime.onMessage.addListener((msg, sender, response) => {
+    if ((msg.from === 'popup') && (msg.subject === 'DOMInfo')) {
+      var domInfo = {
+        url: document.querySelector("#content > div > div > div.l-hero-banner > div > div.fullHero__foreground.systemPlaylistHero__foreground > div.fullHero__artwork.systemPlaylistHero__artwork > div > div > span").style.backgroundImage.split('"')[1]
+      };
+      response(domInfo);
+    }
+  });
--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-import './content-script';
-window.alert(getImage());
-document.getElementById('btn').addEventListener("click", logRes);
-
+// document.getElementById('btn').addEventListener("click", downloadArtwork);
+// function downloadArtwork() {
+//     let url = localStorage.getItem('sc-artwork-url');
+//     let dBtn = document.getElementById('dowdownload-url');
+//     window.alert(url);
+// }
+const setDOMInfo = info => {
+    document.getElementById('url').innerHTML = info.url;
+  };
+window.addEventListener('DOMContentLoaded', () => {
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, tabs => {
+      chrome.tabs.sendMessage(
+          tabs[0].id,
+          {from: 'popup', subject: 'DOMInfo'},
+          setDOMInfo);
+    });
+  });
