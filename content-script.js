57443d47e449fa32cf72e3aa9be01989cdc85677
Samartha <samarthahm@gmail.com>
2021-11-18 21:28:36 +0530

    removed all bugs while fetching dom info

--- content-script.js
+++ content-script.js
@@ -1,7 +1,3 @@
-// function getImage(){
-//     var res = document.querySelector("#content > div > div > div.l-hero-banner > div > div.fullHero__foreground.systemPlaylistHero__foreground > div.fullHero__artwork.systemPlaylistHero__artwork > div > div > span").style.backgroundImage.split('"')[1];
-//     return res
-// }
 chrome.runtime.sendMessage({
     from: 'content',
     subject: 'showPageAction',
@@ -10,9 +6,9 @@ chrome.runtime.sendMessage({
   chrome.runtime.onMessage.addListener((msg, sender, response) => {
     if ((msg.from === 'popup') && (msg.subject === 'DOMInfo')) {
       var domInfo = {
-        url: document.getElementsByClassName("sc-artwork g-opacity-transition")[1].style.backgroundImage.split('"')[1],
-        song: document.getElementsByClassName('soundTitle__title')[0].innerText,
-        artist: document.getElementsByClassName('soundTitle__username ')[0].innerText
+        url: document.getElementsByClassName("sc-artwork sc-artwork-4x image__full g-opacity-transition")[0].style.backgroundImage.split('"')[1].replace('50x50','500x500'),
+        song: document.getElementsByClassName('playbackSoundBadge__title')[0].innerText.split("\n")[1],
+        artist: document.getElementsByClassName('playbackSoundBadge__lightLink')[0].innerText
       };
       response(domInfo);
     }
