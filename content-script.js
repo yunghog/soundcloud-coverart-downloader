02ce3c83509fe1ea3d76abb8068ff7b132373e28
Samartha <samarthahm@gmail.com>
2021-11-19 20:52:59 +0530

    added invalid usage msg

--- content-script.js
+++ content-script.js
@@ -5,12 +5,21 @@ chrome.runtime.sendMessage({
   
   chrome.runtime.onMessage.addListener((msg, sender, response) => {
     if ((msg.from === 'popup') && (msg.subject === 'DOMInfo')) {
+     try {
       var domInfo = {
         url: document.getElementsByClassName("sc-artwork sc-artwork-4x image__full g-opacity-transition")[0].style.backgroundImage.split('"')[1].replace('50x50','500x500'),
         song: document.getElementsByClassName('playbackSoundBadge__title')[0].innerText.split("\n")[1],
         artist: document.getElementsByClassName('playbackSoundBadge__lightLink')[0].innerText
       };
       response(domInfo);
+     } catch (error) {
+      var domInfo = {
+        url: '',
+        song: '',
+        artist: ''
+      };
+      response(domInfo);
+     }
     }
   });
 
--- main.js
+++ main.js
@@ -3,6 +3,17 @@ const setDOMInfo = info => {
     document.getElementById('song').innerHTML = info.song;
     document.getElementById('download-url').href = info.url;
     document.getElementById('cover-art').src = info.url;
+    if(info.artist == '' || info.song == '' || info.url == ''){
+      // showMsg('Please load a song to get the artwork.');
+      document.getElementById('info-content').style.display='none';
+    document.getElementById('msg-content').style.display='block';
+    document.getElementById('msg').innerHTML='Please load a song from SoundCloud to get the artwork.';
+    }
+  };
+  const showMsg = msg =>{
+    document.getElementById('info-content').style.display='none';
+    document.getElementById('msg-content').style.display='block';
+    document.getElementById('msg').innerHTML=msg;
   };
 window.addEventListener('DOMContentLoaded', () => {
     chrome.tabs.query({
